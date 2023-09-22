274e74cd02f232de97baa2d916b6c2933c0ec41a
unknown <bayram43kocaman@gmail.com>
2023-09-22 17:36:52 +0400

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png"alt=""></img>
+      <img src={logo}alt=""></img>
+    </div>
+  );
+}
+
+export default App;
